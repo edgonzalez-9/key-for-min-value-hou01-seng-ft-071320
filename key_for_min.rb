98cf258b448bd1ab4b9a7970c392399c0a0e63e4
Eduardo Gonzalez <edgonzalezhernandez9@gmail.com>
2020-07-01 04:18:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(hash)
-  answer_k = 
+  answer_k = nil 
+  answer_v = nil
+  hash.each_pair do |key, value|
+    if(!answer_k or answer_v > value)
+      answer_v == value
+      answer_k == key
+    end
+  end
+  
+  answer_k
 
 
 end
